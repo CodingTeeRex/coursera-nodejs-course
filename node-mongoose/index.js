bc72c04e18704bff48931415f414d4633be63d52
CodingTeeRex <ivanpjh@gmail.com>
2022-05-03 22:40:42 +0800

    mongoose p2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -16,10 +16,23 @@ connect.then((db) => {
         .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({}).exec();
-        }).then((dishes) => {
-            console.log(dishes);
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set:{ description: "updated test" }
+            }, {
+                new: true 
+            }).exec();
 
+        }).then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: "I\'m getting a sinking feeling!",
+                author: "Leonardo di Carpaccio"
+            })
+            return dish.save();
+        }).then((dish) => {
+            console.log(dish);
             return Dishes.remove({});
         }).then(() => {
             return mongoose.connection.close();
@@ -27,4 +40,3 @@ connect.then((db) => {
             console.log(err);
         });
 })
-
